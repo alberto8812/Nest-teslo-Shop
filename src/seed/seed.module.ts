70d1bdf24610bae2a038a42e53b66a35ea041458
alberto8812 <cvelascosaavedra@gamil.com>
2024-09-08 15:16:24 -0500

    create seed user

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from 'src/products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/auth/entities/user.entity';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService],
-  imports:[ProductsModule]
+  providers: [SeedService,],
+  imports: [ProductsModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ])
+  ]
 })
-export class SeedModule {}
+export class SeedModule { }
