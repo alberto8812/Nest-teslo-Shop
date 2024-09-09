e649581f1e0fdd643167e562bab42c26a326a009
alberto8812 <cvelascosaavedra@gamil.com>
2024-09-08 21:00:37 -0500

    implment socket io

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 
 
@@ -27,7 +28,8 @@ import { AuthModule } from './auth/auth.module';
     CommonModule,
     SeedModule,
     FilesModule,
-    AuthModule
+    AuthModule,
+    MessagesWsModule
   ],
 })
 export class AppModule {}
